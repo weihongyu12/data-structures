b012351d0195a7076e3580c691748c438a0d2c7f
weihongyu12 <weihongyu12@outlook.com>
2017-02-23 20:07:04 +0800

    class Dictionary

---
+++ dictionary.js
@@ -0,0 +1,17 @@
+/**
+ * @file 字典
+ * @author weihongyu <weihongyu12@126.com>
+ * @version 1.0.0
+ */
+
+/**
+ * 字典class
+ * *字典功能已经由ECMAScript6实现
+ * @see ECMAScript 6 入门{@link http://es6.ruanyifeng.com/#docs/set-map#Map}
+ * @see MDN{@link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Map}
+ */
+class Dictionary {
+  constructor() {
+    return new Map();
+  }
+}
