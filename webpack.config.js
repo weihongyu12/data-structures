4a148630a598cacfbe5b0c54722d8a075bb3a73f
weihongyu12 <weihongyu12@outlook.com>
2017-02-24 10:02:17 +0800

    add .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,7 @@ module.exports = {
       {
         test   : /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader : 'babel-loader',
-        query  : {
-          presets: ['env']
-        }
+        loader : 'babel-loader'
       }
     ]
   },
