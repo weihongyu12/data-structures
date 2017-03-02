4ebfac27aa8a8f8f174aa39513a82fe3c9624239
weihongyu12 <weihongyu12@outlook.com>
2017-03-02 20:30:17 +0800

    class Set

---
+++ src/set.js
@@ -0,0 +1,12 @@
+/**
+ * @file 集合
+ * @author weihongyu <weihongyu12@126.com>
+ * @version 1.0.0
+ */
+
+/**
+ * 字典class
+ * *字典功能已经由ECMAScript6实现
+ * @see ECMAScript 6 入门{@link http://es6.ruanyifeng.com/#docs/set-map#Set}
+ * @see MDN{@link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Set}
+ */
