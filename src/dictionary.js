bb256a37c5a8800cce16528d770c0c34cc81e43a
weihongyu12 <weihongyu12@outlook.com>
2017-07-27 16:17:30 +0800

    Dictionary is deprecated,use Map instead

--- src/dictionary.js
+++ src/dictionary.js
@@ -6,7 +6,7 @@
 
 /**
  * 字典class
- * *字典功能已经由ECMAScript6实现
+ * @deprecated 字典功能已经由ECMAScript6实现
  * @see ECMAScript 6 入门{@link http://es6.ruanyifeng.com/#docs/set-map#Map}
  * @see MDN{@link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Map}
  */
